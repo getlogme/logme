ef91fa5c89bebe50743b7bc368aae5936bda7120
getlogme <mekala.madhu@gmail.com>
2016-01-26 22:23:18 -0600

    logme
    Intitial

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+// user model
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    passportLocalMongoose = require('passport-local-mongoose');
+
+
+var User = new Schema({
+  username: String,
+  email:String,
+  password: String
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('users', User);
